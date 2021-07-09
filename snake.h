ce4066bc4a194e9278ce484271e99eac1ef64977
u201922331 <64971585+u201922331@users.noreply.github.com>
2021-07-09 18:51:02 -0500

    Add files via upload

--- snake.h
+++ snake.h
@@ -22,20 +22,25 @@ class Game {
 	Files* files;
 	Shader* gameShader;
 
+	i32 w, h;
+
 	// Muros
 	Model* block;
 	std::vector<v3> wall_pos;
 
 	// Serpiente
 	std::vector<v3> snake;
-	
+
+	// Comida
+	std::vector<v3> food;
+	std::vector<Hitbox*> food_htb;
 public:
 	std::vector<Hitbox*> wall_htb;
 	std::vector<Hitbox*> snake_body;
 public:
 	v3 direction = L;
 
-	Game(i32 w = 10, i32 h = 10) :
+	Game(i32 w = 10, i32 h = 10) : w(w), h(h),
 		files(new Files("resources/shaders", "resources/textures", "resources/objects"))
 	{
 		gameShader = new Shader(files, "shader.vert", "shader.frag");
@@ -55,17 +60,28 @@ public:
 
 	void Update(f32 frame) {
 		if (i32(frame * 10000) % 10 == 0) {
+			auto aux = snake;
 			for (u32 i = 0; i < snake.size(); i++) {
-				snake[i] += 2.0f * direction;
+				if (i == 0) snake[i] += 2.0f * direction;
+				else
+					snake[i] = aux[i - 1];
 				snake_body[i]->Update(snake[i], v3(1.95f));
 			}
-			//snake[0] += 2.0f * direction;
-			
-			/*for (u32 i = 0; i < wall_htb.size(); i++) {
-				std::cout << snake_body[0]->IntersectsWith(*wall_htb[i]) << '\n';
-			}*/
 		}
 
+		for (u32 i = 0; i < food.size(); i++) {
+			if (snake_body[0]->IntersectsWith(*food_htb[i])) {
+				food.erase(food.begin() + i);
+				food_htb.erase(food_htb.begin() + i);
+				break;
+			}
+		}
+
+		if (food.size() < 3 && rand() % 100 < 10) {
+			v3 p = { 2 * rand() % (w + 1) + 1, 0.0f, 2 * rand() % (h + 1) + 1 };
+			food.push_back(p);
+			food_htb.push_back(new Hitbox(p, v3(0.3f)));
+		}
 	}
 
 	void Display(Cam camera, f32 aspect) {
@@ -82,6 +98,13 @@ public:
 		for (u32 i = 0; i < snake.size(); i++) {
 			gameShader->setI32("num", i);
 			block->Draw(gameShader, camera, aspect, snake[i]);
+		}
+
+		// Comida
+		gameShader->use();
+		gameShader->setI32("mode", 2);
+		for (u32 i = 0; i < food.size(); i++) {
+			block->Draw(gameShader, camera, aspect, food[i], v3(0.3));
 		}
 	}
 };
